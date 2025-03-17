78fe8636b825c74d726919d67586c7e358808252
Andrew <Andrewcunningham9911@hotmail.com>
2025-03-17 15:50:52 +0000

    Changed controller. Added Dto and view all ratings

--- WebFrameworksGroupCA2Project/Controllers/RatingsController.cs
+++ WebFrameworksGroupCA2Project/Controllers/RatingsController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using WebFrameworksGroupCA2Project.Data;
+using WebFrameworksGroupCA2Project.DTOs;
 using WebFrameworksGroupCA2Project.Models;
 
 namespace WebFrameworksGroupCA2Project.Controllers
@@ -15,16 +17,36 @@ namespace WebFrameworksGroupCA2Project.Controllers
     [Authorize]
     public class RatingsController : Controller
     {
+
+        private readonly UserManager<AppUser> _userManager;
+
+        private readonly IWebHostEnvironment environment;
+
         private readonly WebFrameworksGroupCA2ProjectContext _context;
 
-        public RatingsController(WebFrameworksGroupCA2ProjectContext context)
+        public RatingsController(WebFrameworksGroupCA2ProjectContext context, UserManager<AppUser> userManager, IWebHostEnvironment environment)
         {
             _context = context;
+            _userManager = userManager;
+            this.environment = environment;
         }
 
-        // GET: Ratings
+
+        // GET: RatingsUser
         public async Task<IActionResult> Index()
         {
+
+            var userid = _userManager.GetUserId(HttpContext.User);
+
+            var webFrameworksGroupCA2ProjectContext = _context.Rating.Include(r => r.Song).Include(r => r.User).Where(x => x.UserId == userid);
+            return View(await webFrameworksGroupCA2ProjectContext.ToListAsync());
+        }
+
+        // GET: RatingsUser
+        public async Task<IActionResult> ViewAllRatings()
+        {
+
+
             var webFrameworksGroupCA2ProjectContext = _context.Rating.Include(r => r.Song).Include(r => r.User);
             return View(await webFrameworksGroupCA2ProjectContext.ToListAsync());
         }
@@ -62,8 +84,18 @@ namespace WebFrameworksGroupCA2Project.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,UserRating,Comment,UserId,SongId")] Rating rating)
+        public async Task<IActionResult> Create(RatingPostDTO ratingDTO)
         {
+            var userId = _userManager.GetUserId(HttpContext.User);
+
+            Rating rating = new Rating()
+            {
+                UserRating = ratingDTO.UserRating,
+                Comment = ratingDTO.Comment,
+                UserId = userId,
+                SongId = ratingDTO.SongId
+
+            };
             if (ModelState.IsValid)
             {
                 _context.Add(rating);
