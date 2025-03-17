29b975e1d3722def65ffff727bda34ff3f2501fb
TobyZedomi <tobyzedo4@gmail.com>
2025-03-17 13:50:14 +0000

    Add project files.

---
+++ WebFrameworksGroupCA2Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebFrameworksGroupCA2Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
