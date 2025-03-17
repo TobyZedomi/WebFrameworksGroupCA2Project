409c099cc8cf6fec4d3226f3c280be0fce358224
TobyZedomi <tobyzedo4@gmail.com>
2025-03-17 16:55:32 +0000

    Playlist get, post and put dto. playlistsong get dto

---
+++ WebFrameworksGroupCA2Project/DTOs/PlaylistPutDTO.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using WebFrameworksGroupCA2Project.Models;
+
+namespace WebFrameworksGroupCA2Project.DTOs
+{
+    public class PlaylistPutDTO
+    {
+
+        [Key]
+
+        public int Id { get; set; }
+
+        [RegularExpression(@"^[A-Z0-9]+[a-zA-Z0-9\s]*$", ErrorMessage = "Playlist Name must Start with a capital Letter or number. Numbers and letters only, 30 characters maximum"), Required, StringLength(30)]
+        public string PlaylistName { get; set; }
+
+        public bool StatusPrivate { get; set; }
+
+        public IFormFile? ImageFile { get; set; }
+
+        public string? UserId { get; set; }
+        [ForeignKey("UserId")]
+        public AppUser? AppUser { get; set; }
+
+        public List<PlaylistSong>? PlaylistSongs { get; set; }
+    }
+}
---
+++ WebFrameworksGroupCA2Project/DTOs/PlaylistSongPutDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using WebFrameworksGroupCA2Project.Models;
+
+namespace WebFrameworksGroupCA2Project.DTOs
+{
+    public class PlaylistSongPutDTO
+    {
+
+
+        [Key]
+
+        public int Id { get; set; }
+        public int? PlaylistId { get; set; }
+        [ForeignKey("PlaylistId")]
+        public Playlist? Playlist { get; set; }
+        public int? SongId { get; set; }
+        [ForeignKey("SongId")]
+        public Song? Song { get; set; }
+    }
+}
