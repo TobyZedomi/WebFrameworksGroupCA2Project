6ce0e572fd95654a1d3c2a942dd329c4b3c89849
TobyZedomi <tobyzedo4@gmail.com>
2025-04-12 14:24:55 +0100

    Authorize on vinyl and stock controllers

--- WebFrameworksGroupCA2Project/Controllers/VinylsController.cs
+++ WebFrameworksGroupCA2Project/Controllers/VinylsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,8 @@ using WebFrameworksGroupCA2Project.Models;
 
 namespace WebFrameworksGroupCA2Project.Controllers
 {
+
+    [Authorize]
     public class VinylsController : Controller
     {
         private readonly WebFrameworksGroupCA2ProjectContext _context;
